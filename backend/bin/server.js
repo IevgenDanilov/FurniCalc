c47e63861394da74277cc6fb4db3bb4677bde5cd
Yevhen Danilov <danilov.evgen@gmail.com>
2022-08-12 14:34:13 +0300

    mongoose add

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,3 +1,4 @@
+const { string, number } = require("joi");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -5,12 +6,32 @@ const app = require("../app");
 
 const { DB_HOST, PORT = 4000 } = process.env;
 
+const { Schema, model } = mongoose;
+
+const moduleSchema = Schema({
+  name: String,
+  length: Number,
+});
+
+const Module = model("module", moduleSchema);
+
+const newModule = {
+  name: "example",
+  length: 100,
+};
+
 mongoose
   .connect(DB_HOST)
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    })
+  .then(
+    () => {
+      Module.create(newModule, (error, data) => {
+        console.log("error", error);
+        console.log("data", data);
+      });
+    }
+    // app.listen(PORT, () => {
+    //   console.log(`Server running on port ${PORT}`);
+    // })
   )
   .catch((error) => {
     console.log(error.message);
