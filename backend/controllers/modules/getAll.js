910a3070e3cc273005f920ae127063caf2bb67ea
Yevhen Danilov <danilov.evgen@gmail.com>
2022-08-14 09:40:57 +0300

    filters on data add

--- backend/controllers/modules/getAll.js
+++ backend/controllers/modules/getAll.js
@@ -2,7 +2,10 @@ const { Module } = require("../../models");
 const { sendSuccessReq } = require("../../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Module.find({});
+  const result = await Module.find(
+    {},
+    "projectName category mName mWidth mDepth mHeight mQuantity detaling"
+  );
   sendSuccessReq(res, { result });
 };
 
